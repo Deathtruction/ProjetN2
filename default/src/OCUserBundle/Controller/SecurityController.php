963fd0b64092e7de70ce33ae0cd5e67ad2304cdd
Deathtruction <kevin_mongault@orange.fr>
2017-03-10 12:32:37 +0100

    ùymlikuy

---
+++ default/src/OCUserBundle/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OCUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+
+{
+    public function loginAction(Request $request)
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+
+            return $this->redirectToRoute('logge');
+        }
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('login.html.twig', array(
+
+            'last_username' => $authenticationUtils->getLastUsername(),
+
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+}
+
