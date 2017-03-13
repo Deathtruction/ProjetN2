56bd18c586f66e7d42f74632d1d34d47e35e7584
christo169 <christopher.ol@hotmail.fr>
2017-03-13 11:49:19 +0100

    ajout fiche d'inscription

--- default/src/OCUserBundle/Controller/AuthController.php
+++ default/src/OCUserBundle/Controller/AuthController.php
@@ -12,6 +12,6 @@ class AuthController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('');
+        return $this->render('::inscription.html.twig');
     }
 }
