f98efe4928a50d2bd0d1d4c9ddfda546a95cc48c
Deathtruction <kevin_mongault@orange.fr>
2017-03-13 15:36:31 +0100

    Maj

---
+++ default/src/OCUserBundle/Controller/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OCUserBundle\Controller;
+
+use OCUserBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RegisterController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+
+
+    /**
+     * @Route("/register")
+     */
+    public function load()
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+
+        $user = new User;
+        $user->setUsername('test');
+        $user->setPassword('test');
+        $user->setSalt('');
+        $user->setRoles(array('ROLE_USER'));
+        $em->persist($user);
+
+        $em->flush();
+
+    }
+}
--- default/src/OCUserBundle/Controller/SecurityController.php
+++ default/src/OCUserBundle/Controller/SecurityController.php
@@ -2,25 +2,28 @@
 
 namespace OCUserBundle\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use OCUserBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 
 {
+
+
+
     public function loginAction(Request $request)
     {
+
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
 
             return $this->redirectToRoute('logge');
         }
-
         $authenticationUtils = $this->get('security.authentication_utils');
-
         return $this->render('login.html.twig', array(
-
             'last_username' => $authenticationUtils->getLastUsername(),
-
             'error'         => $authenticationUtils->getLastAuthenticationError(),
         ));
     }
